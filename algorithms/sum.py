6b3bda4c17c07c7ae449cd38e8efd8a59a1257fc
Somonor Hong <150505791+somonor011@users.noreply.github.com>
2024-03-04 23:14:45 +0700

    Add files via upload

---
+++ algorithms/sum.py
@@ -0,0 +1,7 @@
+# create algorithm sum
+num1 = int(input("Input value num1 : "))
+num2 = int(input("Input value num2 : "))
+
+sum = num1 + num2
+
+print(f"num1 + num2  = {sum}")
---
+++ algorithms/sumwfunc.py
@@ -0,0 +1,7 @@
+#function sum a + b
+
+def SUM(a,b):
+    print(f"a + b = {a+b}")
+    return a+b
+
+SUM(10,20)
